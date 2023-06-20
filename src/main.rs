a038a076c63fe25bd14e7e44418b5111e9361a19
Julia <juliariety@gmail.com>
2023-06-20 21:39:37 +0200

    Implement niladic `PING` command
    The implementation's quite a mess, I'll clean it up later.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::str;
 use std::thread;
+
+use array::{Array, Value};
+use bulk_string::BulkString;
 
 use crate::array::parse;
 
@@ -20,11 +24,41 @@ fn handle_client(mut stream: TcpStream) {
                 }
 
                 if let Ok(result) = parse(&buf[..n]) {
-                    println!("{:#?}", result);
+                    match result {
+                        Array::Null => stream.write_all(b"-ERR unexpected null array\r\n").unwrap(),
+                        Array::Empty => stream
+                            .write_all(b"-ERR unexpected empty array\r\n")
+                            .unwrap(),
+                        Array::Filled(values) => match &values[0] {
+                            Value::BulkString(bs) => match bs {
+                                BulkString::Null => stream
+                                    .write_all(b"-ERR unexpected null bulk string\r\n")
+                                    .unwrap(),
+                                BulkString::Empty => stream
+                                    .write_all(b"-ERR unexpected empty bulk string\r\n")
+                                    .unwrap(),
+                                BulkString::Filled(command) => {
+                                    let command = match str::from_utf8(command) {
+                                        Ok(command) => command,
+                                        Err(_) => {
+                                            stream.write_all(b"-ERR invalid command\r\n").unwrap();
+                                            continue;
+                                        }
+                                    };
 
-                    stream.write_all(b"+OK\r\n").unwrap();
+                                    match command.to_uppercase().as_str() {
+                                        "PING" => {
+                                            // TODO: Implement diadic version of PING: https://redis.io/commands/ping/
+                                            stream.write_all(b"+PONG\r\n").unwrap()
+                                        }
+                                        _ => stream.write_all(b"-ERR unknown command\r\n").unwrap(),
+                                    };
+                                }
+                            },
+                        },
+                    };
                 } else {
-                    stream.write_all(b"-ERR An error occurred\r\n").unwrap();
+                    stream.write_all(b"-ERR an error occurred\r\n").unwrap();
                 }
             }
             Err(_) => return,
