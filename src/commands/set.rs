1ab63b11b9e5b0e10cbe34adc1ab3bcf1c75c99f
Julia <juliariety@gmail.com>
2023-06-26 14:50:12 +0200

    Refactor `SET` option handling

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -6,6 +6,18 @@ use crate::bulk_string::BulkString;
 
 pub(crate) struct Set;
 
+#[derive(PartialEq)]
+enum SetOption {
+    NotSpecified,
+    IfExists,
+    IfNotExists,
+}
+
+enum GetOption {
+    NotSpecified,
+    Get,
+}
+
 impl Command for Set {
     fn execute(&self, data: &mut Data, arguments: &[Value]) -> Response {
         if !(2..=4).contains(&arguments.len()) {
@@ -27,9 +39,8 @@ impl Command for Set {
         };
 
         // TODO: Implement more options: https://redis.io/commands/set.
-        let mut set_if_exist = false;
-        let mut set_if_not_exist = false;
-        let mut get = false;
+        let mut set_option = SetOption::NotSpecified;
+        let mut get_option = GetOption::NotSpecified;
 
         for argument in &arguments[2..] {
             let arg = match argument {
@@ -44,78 +55,80 @@ impl Command for Set {
             };
 
             match decoded.as_str() {
-                "XX" => set_if_exist = true,
-                "NX" => set_if_not_exist = true,
-                "GET" => get = true,
+                "XX" => {
+                    set_option = if set_option == SetOption::NotSpecified {
+                        SetOption::IfExists
+                    } else {
+                        return Response::Error("'XX' and 'NX' can't be used at the same time");
+                    }
+                }
+                "NX" => {
+                    set_option = if set_option == SetOption::NotSpecified {
+                        SetOption::IfNotExists
+                    } else {
+                        return Response::Error("'XX' and 'NX' can't be used at the same time");
+                    }
+                }
+                "GET" => get_option = GetOption::Get,
                 // TODO: It would be nice to be able to use non-static strings in errors, so we could do:
                 // _ => return Response::Error(format!("'{decoded}' is not a valid option")),
                 _ => return Response::Error("invalid option"),
             };
-
-            if set_if_exist && set_if_not_exist {
-                return Response::Error("'XX' and 'NX' can't be used at the same time");
-            }
         }
 
-        if set_if_exist {
-            if data.contains_key(key) {
-                let old_value = data.get(key).unwrap().clone();
+        match (set_option, get_option) {
+            (SetOption::NotSpecified, GetOption::NotSpecified) => {
+                data.insert(key.clone(), value.clone());
+
+                Response::SimpleString("OK")
+            }
+            (SetOption::NotSpecified, GetOption::Get) => {
+                let old_value = data.get(key).unwrap_or(&BulkString::Null).clone();
 
                 data.insert(key.clone(), value.clone());
 
-                if get {
+                Response::BulkString(old_value)
+            }
+            (SetOption::IfExists, GetOption::NotSpecified) => {
+                if data.contains_key(key) {
+                    data.insert(key.clone(), value.clone());
+
+                    return Response::SimpleString("OK");
+                }
+
+                Response::BulkString(BulkString::Null)
+            }
+            (SetOption::IfExists, GetOption::Get) => {
+                if data.contains_key(key) {
+                    let old_value = data.get(key).unwrap().clone();
+
+                    data.insert(key.clone(), value.clone());
+
                     return Response::BulkString(old_value);
                 }
 
-                return Response::SimpleString("OK");
+                Response::BulkString(BulkString::Null)
             }
+            (SetOption::IfNotExists, GetOption::NotSpecified) => {
+                if !data.contains_key(key) {
+                    data.insert(key.clone(), value.clone());
 
-            return Response::BulkString(BulkString::Null);
-        }
-
-        if set_if_not_exist {
-            let mut was_inserted = false;
-            let mut current_value = None;
-
-            if !data.contains_key(key) {
-                was_inserted = true;
-
-                data.insert(key.clone(), value.clone());
-            } else {
-                current_value = Some(data.get(key).unwrap().clone());
-            }
-
-            return if get {
-                if let Some(current_value) = current_value {
-                    Response::BulkString(current_value)
-                } else {
-                    Response::BulkString(BulkString::Null)
+                    return Response::SimpleString("OK");
                 }
-            } else if was_inserted {
-                Response::SimpleString("OK")
-            } else {
+
                 Response::BulkString(BulkString::Null)
-            };
-        }
-
-        if get {
-            let mut old_value = None;
-
-            if data.contains_key(key) {
-                old_value = Some(data.get(key).unwrap().clone());
             }
+            (SetOption::IfNotExists, GetOption::Get) => {
+                if !data.contains_key(key) {
+                    data.insert(key.clone(), value.clone());
 
-            data.insert(key.clone(), value.clone());
+                    return Response::BulkString(BulkString::Null);
+                }
 
-            if let Some(old_value) = old_value {
+                let old_value = data.get(key).unwrap().clone();
+
                 Response::BulkString(old_value)
-            } else {
-                Response::BulkString(BulkString::Null)
             }
-        } else {
-            data.insert(key.clone(), value.clone());
-
-            Response::SimpleString("OK")
         }
     }
 }
@@ -123,6 +136,36 @@ impl Command for Set {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn no_options() {
+        let mut data = Data::new();
+
+        let arguments = [
+            Value::BulkString(BulkString::Filled(b"key".to_vec())),
+            Value::BulkString(BulkString::Filled(b"value".to_vec())),
+        ];
+
+        let response = Set.execute(&mut data, &arguments);
+
+        assert!(matches!(response, Response::SimpleString("OK")));
+
+        let arguments = [
+            Value::BulkString(BulkString::Filled(b"key".to_vec())),
+            Value::BulkString(BulkString::Filled(b"value2".to_vec())),
+        ];
+
+        let response = Set.execute(&mut data, &arguments);
+
+        assert_eq!(
+            BulkString::Filled(b"value2".to_vec()),
+            data.get(&BulkString::Filled(b"key".to_vec()))
+                .cloned()
+                .unwrap()
+        );
+
+        assert!(matches!(response, Response::SimpleString("OK")));
+    }
 
     #[test]
     fn xx_not_met() {
